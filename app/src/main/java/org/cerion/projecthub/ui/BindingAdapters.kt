dc7bd4c235847ffdc7df2f897ee1485ed9cc1d77
andy <andy@cerion.org>
2020-04-24 10:37:09 -0700

    added formatting for text with emojis

--- app/src/main/java/org/cerion/projecthub/ui/BindingAdapters.kt
+++ app/src/main/java/org/cerion/projecthub/ui/BindingAdapters.kt
@@ -1,11 +1,17 @@
 package org.cerion.projecthub.ui
 
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import org.cerion.projecthub.R
 import org.cerion.projecthub.model.Card
 import org.cerion.projecthub.model.IssueCard
 import org.cerion.projecthub.model.NoteCard
+
 
 @BindingAdapter("cardImage")
 fun ImageView.setCardImage(card: Card) {
@@ -22,3 +28,37 @@ fun ImageView.setCardImage(card: Card) {
 
     setImageDrawable(context.getDrawable(id))
 }
+
+
+@BindingAdapter("formattedText")
+fun formattedText(textView: TextView, text: String) {
+    // Replace markdown emojis
+    val sb = SpannableStringBuilder(replaceMarkdown(text))
+    var start = 0
+
+    // Bold text formatting
+    while(true) {
+        val tempText = sb.toString()
+        start = tempText.indexOf("**", start)
+        val end = tempText.indexOf("**", start + 1)
+        if (start == -1 || end == -1)
+            break
+
+        sb.setSpan(StyleSpan(Typeface.BOLD), start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        sb.replace(start, start+2, "")
+        sb.replace(end-2, end, "")
+        start = end + 2
+    }
+
+    textView.text = sb
+}
+
+// TODO more efficient way of doing this by searching for tags rather than 100+ replaces
+fun replaceMarkdown(str: String): String {
+    var result = str
+    result = result.replace(":sparkles:", getEmojiByUnicode(0x2728))
+
+    return result
+}
+
+fun getEmojiByUnicode(unicode: Int) = String(Character.toChars(unicode))
