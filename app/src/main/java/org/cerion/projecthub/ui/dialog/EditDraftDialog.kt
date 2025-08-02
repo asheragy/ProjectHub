fd4fb7268699666ec4ae43c5cd726fc59c68b077
Andrew Sheragy <andy@cerion.org>
2025-08-01 17:59:59 -0700

    finished edit draft screen

--- app/src/main/java/org/cerion/projecthub/ui/dialog/EditDraftDialog.kt
+++ app/src/main/java/org/cerion/projecthub/ui/dialog/EditDraftDialog.kt
@@ -4,14 +4,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -21,25 +29,36 @@ import androidx.compose.ui.unit.sp
 import org.cerion.projecthub.ui.AppTheme
 
 
+data class EditDraftDialogState(
+    val title: String = "",
+    val body: String = "",
+)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditDraftDialog(
-    title: String,
-    body: String,
-    onTitleChange: (String) -> Unit,
-    onBodyChange: (String) -> Unit,
-    onSaveClick: () -> Unit
+    initialState: EditDraftDialogState,
+    onDismiss: (EditDraftDialogState?) -> Unit
 ) {
+    var state by remember { mutableStateOf(initialState) }
+
     Surface(color = MaterialTheme.colorScheme.surface) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
         ) {
-
             TopAppBar(
                 title = { Text("Edit Draft") },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                actions = {
+                    IconButton(onClick = { onDismiss(null) }) {
+                        Icon(
+                            imageVector = Icons.Default.Close, // or use a custom painterResource
+                            contentDescription = "Close"
+                        )
+                    }
+                },
             )
 
             Spacer(modifier = Modifier.height(8.dp))
@@ -52,8 +71,10 @@ fun EditDraftDialog(
             )
 
             OutlinedTextField(
-                value = title,
-                onValueChange = onTitleChange,
+                value = state.title,
+                onValueChange = {
+                    state = state.copy(title = it)
+                },
                 modifier = Modifier.fillMaxWidth(),
                 placeholder = { Text("Title") },
                 singleLine = true,
@@ -69,8 +90,10 @@ fun EditDraftDialog(
             )
 
             OutlinedTextField(
-                value = body,
-                onValueChange = onBodyChange,
+                value = state.body,
+                onValueChange = {
+                    state = state.copy(body = it)
+                },
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxWidth(),
@@ -81,7 +104,9 @@ fun EditDraftDialog(
             Spacer(modifier = Modifier.height(16.dp))
 
             Button(
-                onClick = onSaveClick,
+                onClick = {
+                    onDismiss(state)
+                },
                 modifier = Modifier.align(Alignment.Start)
             ) {
                 Text("Save")
@@ -95,6 +120,6 @@ fun EditDraftDialog(
 @Composable
 fun EditDraftDialogPreview() {
     AppTheme {
-        EditDraftDialog("New Draft", "Some body text", { _ -> }, { _ -> }) { }
+        EditDraftDialog(EditDraftDialogState("New Draft", "some body text")) { }
     }
 }
--- app/src/main/java/org/cerion/projecthub/ui/dialog/EditDraftDialogFragment.kt
+++ app/src/main/java/org/cerion/projecthub/ui/dialog/EditDraftDialogFragment.kt
@@ -1,16 +1,13 @@
 package org.cerion.projecthub.ui.dialog
 
 import EditDraftDialog
-import android.app.Dialog
+import EditDraftDialogState
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.DialogFragment
-import org.cerion.projecthub.R
-import org.cerion.projecthub.databinding.DialogEditDraftBinding
 import org.cerion.projecthub.model.DraftIssueCard
 import org.cerion.projecthub.ui.AppTheme
 import org.cerion.projecthub.ui.project.ProjectHomeViewModel
@@ -26,57 +23,39 @@ class EditDraftDialogFragment : DialogFragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        /*
-        val binding = DialogEditDraftBinding.inflate(layoutInflater, container, false)
-
         val args = EditDraftDialogFragmentArgs.fromBundle(requireArguments())
         val viewModel = projectViewModel.columns.value!![args.columnIndex]
 
         val isNew = args.cardId == ""
         var contentId = ""
+        var initialState = EditDraftDialogState("", "")
 
         viewModel.cards.value!!.firstOrNull { it.itemId == args.cardId }?.let {
             it as DraftIssueCard
-            binding.title.setText(it.title)
-            binding.body.setText(it.body)
+            initialState = EditDraftDialogState(title = it.title, body = it.body)
             contentId = it.id
         }
-
-        binding.save.setOnClickListener {
-            val title = binding.title.text.toString()
-            val body = binding.body.text.toString()
-
-            if (isNew) {
-                val card = DraftIssueCard.create(title, body)
-                viewModel.addDraft(card)
-            }
-            // TODO check if data changed
-            else {
-                val card = DraftIssueCard(args.cardId, contentId, title, body)
-                viewModel.updateDraft(card)
-            }
-
-            dismiss()
-        }
-
-        binding.toolbar.inflateMenu(R.menu.edit_note)
-        binding.toolbar.menu.getItem(0).setOnMenuItemClickListener {
-            dismiss()
-            true
-        }
-
-        return binding.root
-         */
 
         return ComposeView(requireContext()).apply {
             setContent {
                 AppTheme {
                     EditDraftDialog(
-                        title = "Initial Title",
-                        body = "Initial Body",
-                        onTitleChange = { /* handle title update */ },
-                        onBodyChange = { /* handle body update */ },
-                        onSaveClick = {
+                        initialState,
+                        onDismiss = { finalState ->
+                            if (finalState != null) {
+                                if (isNew) {
+                                    val card = DraftIssueCard.create(finalState.title, finalState.body)
+                                    viewModel.addDraft(card)
+                                }
+                                else if (finalState == initialState) {
+                                    println("Draft unchanged")
+                                }
+                                else {
+                                    val card = DraftIssueCard(args.cardId, contentId, finalState.title, finalState.body)
+                                    viewModel.updateDraft(card)
+                                }
+                            }
+
                             dismiss()
                         }
                     )
